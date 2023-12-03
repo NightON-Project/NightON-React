4f8ac4c0e5104a1657ead8b77d01c3bdd834732e
MalicknND <ndiayemalick062@gmail.com>
2023-12-03 16:21:25 +0100

    Navbar & Footer

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import styles from "./index.module.scss";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import LanguageRoundedIcon from "@mui/icons-material/LanguageRounded";
 import BasicMenu from "./ProfileMenu";
+import logo from "../../assets/img/logo.png";
 
 const index = () => {
   return (
     <div className={styles.navbar}>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1280px-Airbnb_Logo_B%C3%A9lo.svg.png"
-        alt="Airbnb"
-        className={styles.navbar_logo}
-      />
+      <img src={logo} alt="Airbnb" className={styles.navbar_logo} />
       <div className={styles.search_bar}>
         <div className={styles.search_bar_text}>Any Where</div>
         <div className={styles.search_bar_text}>Any Week</div>
@@ -22,9 +18,7 @@ const index = () => {
       </div>
       <div className={styles.profile_container}>
         <div className={styles.nighton_home}>NightOn Your Home</div>
-        <div className={styles.nighton_home}>
-          <LanguageRoundedIcon sx={{ fontSize: "1.3rem" }} />
-        </div>
+
         <div className={styles.profile}>
           <BasicMenu />
         </div>
