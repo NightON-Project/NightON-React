8e515bc70087217ec20d00dc5467bb33f2680c45
Damkeus <dylan.prochettemercure@ynov.com>
2023-12-05 12:23:55 +0100

    lol
    lol

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,82 +1,66 @@
 import React, { useState } from 'react';
+import './register.css';
 
 function Index() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-    });
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [email, setEmail] = useState('');
+  const [acceptTerms, setAcceptTerms] = useState(false);
 
-    //Mise à jour constante des données entrées par l'utilisateur
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Ici vous pouvez ajouter le code pour gérer l'envoi des données du formulaire
+    console.log(nom, prenom, email, acceptTerms);
+  };
 
-
-    //Envoie de la requête !
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('http://localhost:8080/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-
-
-            //Gestion de la réponse !
-            if (response.ok) {
-                console.log(response.body);
-            } else {
-                const errorResponse = await response.json(); // Récupérer le JSON renvoyé par le serveur
-                console.log(errorResponse.message); // Afficher le message d'erreur côté client
-            }
-        } catch (error) {
-            console.error('Erreur lors de la requête :', error);
-        }
-    };
-
-    return (
-        <div className="registration-container">
-            <h1>Inscription</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Nom :</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Adresse e-mail :</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Mot de passe :</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">S'inscrire</button>
-            </form>
+  return (
+    <div className="container">
+      <div className="register">
+        <div className="box2">
+          <img className='rectangle' alt='rectangle' src='Rectangle.png'/>
         </div>
-    );
+        <header className="register-header">
+          <h1>Que la fête commence !!!</h1>
+          <p>Rejoins Nighton, réserve ton logement et commence à faire la fête dès maintenant !</p>
+          <button>Sign in with Google</button>
+          <div>OR</div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={nom}
+              onChange={(e) => setNom(e.target.value)}
+              placeholder="Nom"
+            />
+            <input
+              type="text"
+              value={prenom}
+              onChange={(e) => setPrenom(e.target.value)}
+              placeholder="Prénom"
+            />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+            <label>
+              <input
+                type="checkbox"
+                checked={acceptTerms}
+                onChange={(e) => setAcceptTerms(e.target.checked)}
+              />
+              J'accepte les termes et conditions
+            </label>
+            <button type="submit">S'inscrire</button>
+          </form>
+        </header>
+      </div>
+      {/* Ajout de la nouvelle div avec le carré blanc */}
+      <div className="square-container">
+        <div className="white-square"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
