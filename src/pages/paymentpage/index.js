58bed121e07557351d0850adb4fa1c4c3f8ddb25
Damkeus <dylan.prochettemercure@ynov.com>
2024-03-06 11:40:48 +0100

    vrez
    vez

---
+++ src/pages/paymentpage/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './index.css';
+import fiest from '../../assets/img/fiest.svg';
+
+function Index() {
+    const [nom, setNom] = useState('');
+    const [prenom, setPrenom] = useState('');
+    const [birthdate, setBirthdate] = useState('');
+    const [adresse, setAdresse] = useState('');
+    const [ville, setVille] = useState('');
+    const [postcode, setPostcode] = useState('');
+    const [acceptTerms, setAcceptTerms] = useState(false); 
+
+ }
