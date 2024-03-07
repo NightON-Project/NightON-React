b27def827ed1789727d022b804a054e5658af8f8
Damkeus <dylan.prochettemercure@ynov.com>
2024-03-07 14:40:40 +0100

    VREZa
    bvrceza

--- src/pages/paymentpage/index.js
+++ src/pages/paymentpage/index.js
@@ -72,11 +72,12 @@ function Index() {
                 <br></br>
               <button className="submit">
                 <div className="movement">
-                   <svg className="fleche" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <g id="Logo">
-                     <path id="Vector" d="M11.3333 7.99992H2M11.3333 7.99992L7.33333 11.9999M11.3333 7.99992L7.33333 3.99992M14 3.33325V12.6666" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                     </g>
-                   </svg>
+                <svg className="fleche" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="Logo" x="10" y="10">
+                    <path id="Vector" d="M11.3333 7.99992H2M11.3333 7.99992L7.33333 11.9999M11.3333 7.99992L7.33333 3.99992M14 3.33325V12.6666" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </g>
+                </svg>
+
                   <span className='straight'>S'inscrire</span>
               </div>
             </button>
